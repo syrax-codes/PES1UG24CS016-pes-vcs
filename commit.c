48a1b7466a067ae7a4be44ed8f23387d380a9422
syrax-codes <abhirajdhananjay@gmail.com>
2026-04-21 11:58:01 -0400

    Phase 4: Basic commit object with message

--- commit.c
+++ commit.c
@@ -194,8 +194,12 @@ int head_update(const ObjectID *new_commit) {
 //
 // Returns 0 on success, -1 on error.
 int commit_create(const char *message, ObjectID *commit_id_out) {
-    // TODO: Implement commit creation
-    // (See Lab Appendix for logical steps)
-    (void)message; (void)commit_id_out;
-    return -1;
+    // simple commit: only message
+    char buffer[1024];
+
+    snprintf(buffer, sizeof(buffer),
+             "message %s\n",
+             message);
+
+    return object_write(OBJ_COMMIT, buffer, strlen(buffer), commit_id_out);
 }
